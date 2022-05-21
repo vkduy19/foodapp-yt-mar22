9fa8bd79a3ed7dcdee99973924e3ce08ee099b89
vkduy19 <vkduy19@apcs.fitus.edu.vn>
2022-05-21 17:10:01 +0700

    update contact responsive and language

--- src/components/ContactContainer.jsx
+++ src/components/ContactContainer.jsx
@@ -2,45 +2,35 @@ import React from "react";
 
 const ContactContainer = () => {
   return (
-    <div class="w-screen h-screen max-w-lg m-auto mt-20">
-      <form class="w-full max-w-lg">
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              class="block uppercase tracking-wide text-textColor text-sm font-bold mb-2"
-              for="grid-first-name"
-            >
-              First Name
-            </label>
-            <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="grid-first-name"
-              type="text"
-              placeholder="Your first name"
-            />
-          </div>
-          <div class="w-full md:w-1/2 px-3">
+    <div>
+      <p className="w-screen max-w-lg lg:max-w-2xl mx-auto mt-5 mb-10 text-[2rem] lg:text-[3rem] font-bold tracking-wide text-headingColor">
+        <span className="text-blue-600">Cần hỗ trợ?</span><br/>
+        Hãy liên hệ với chúng tôi qua đơn bên dưới.
+      </p>
+      <form class="h-screen lg:max-w-2xl max-w-lg mx-auto">
+        <div class="flex flex-wrap mx-3 mb-6">
+          <div class="w-full px-3">
             <label
               class="block uppercase tracking-wide text-textColor text-sm font-bold mb-2"
               for="grid-last-name"
             >
-              Last Name
+              Họ tên
             </label>
             <input
               class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-last-name"
               type="text"
-              placeholder="Your last name"
+              placeholder="Họ tên của bạn"
             />
           </div>
         </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="flex flex-wrap mx-3 mb-6">
           <div class="w-full px-3">
             <label
               class="block uppercase tracking-wide text-textColor text-sm font-bold mb-2"
               for="grid-email"
             >
-              E-mail
+              Email
             </label>
             <input
               class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
@@ -49,13 +39,13 @@ const ContactContainer = () => {
             />
           </div>
         </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="flex flex-wrap mx-3 mb-6">
           <div class="w-full px-3">
             <label
               class="block uppercase tracking-wide text-textColor text-sm font-bold mb-2"
               for="grid-message"
             >
-              Message
+              Lời nhắn
             </label>
             <textarea
               class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
@@ -63,16 +53,15 @@ const ContactContainer = () => {
             />
           </div>
         </div>
-        <div class="md:flex md:items-center">
-          <div class="md:w-1/3">
+        <div class="md:flex md:items-center mx-3">
+          <div class="md:w-1/3 px-3">
             <button
-              class="shadow bg-cartNumBg hover:bg-white hover:text-textColor focus:outline-none focus:ring focus:ring-gray-500 focus:ring-offset-0 text-white font-bold py-2 px-4 rounded"
+              class="shadow bg-cartNumBg hover:bg-white hover:text-cartNumBg focus:outline-none focus:ring focus:ring-gray-500 focus:ring-offset-0 text-white font-bold py-2 px-4 rounded"
               type="button"
             >
-              Send
+              Gửi
             </button>
           </div>
-          <div class="md:w-2/3"></div>
         </div>
       </form>
     </div>
